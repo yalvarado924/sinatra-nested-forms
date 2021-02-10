0950c5c3b692ea33d307b5b51ac50e6eb832e678
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-09 18:46:23 -0600

    commit tree 40910ba7aeb4d9fb3e10bfb5a6481bfc7dfc21f6

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,32 @@
 class Pirate
+        #attr_reader :name, :weight, :height
+      
+        #PIRATES = []
+      #
+       # def initialize(args)
+        #  @name = args[:name]
+         # @weight = args[:weight]
+          #@height = args[:height]
+          #PIRATES << self
+       # end
+      
+        #def self.all
+         # PIRATES
+        #end
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
+
